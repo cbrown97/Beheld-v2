ecb42836a520c3ffc7c82277fbf0c5b6f8dfca45
Colin <colin.scott.brown@gmail.com>
2013-09-07 12:39:54 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeheldV2::Application.config.secret_token = '3059c7c558317021b62bef16b151534d5f679cc5e94b4e8ae227d6d8aaa27f70ce68a75d43a228ad8d28f1482d9218eeb117d5fc829aba5cccb1974306dc6213'
