9ccaebea6c441b9c1d49970c848202c3bb2a77e4
Colin <colin.scott.brown@gmail.com>
2013-09-08 11:34:03 -0700

    Added devise, bootstrap and stylesheets.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,52 @@
+class PagesController < ApplicationController
+ def home
+  end
+
+  def about
+  end
+
+  def search
+  end
+  
+  def search_results
+  end
+  
+  def legal
+  end
+  
+  def selection
+  end
+  
+  def review
+  end
+  
+  def shipping
+  end
+  
+  def payment
+  end
+  
+  def success_payment
+  end
+  
+  def legal
+  end
+  
+  def user_profile
+  end
+  
+  def artist_profile
+  end
+  
+  def artist_search_results
+  end
+  
+  def gallery_profile
+  end
+  
+  def gallery_search_results
+  end
+  
+  def price_search_results
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,39 @@
 BeheldV2::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
+  
+  devise_for :users
 
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  root :to => 'pages#home'
+  
+  get 'about' => 'pages#about'
 
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
+  get 'search' => 'pages#search'
+  
+  get 'price_search_results' => 'pages#price_search_results'
+  
+  get 'artist_search_results' => 'pages#artist_search_results'
+  
+  get 'gallery_search_results' => 'pages#gallery_search_results'
+  
+  get 'learn' => 'pages#learn'
+  
+  get 'blog' => 'pages#blog'
+  
+  get 'legal' => 'pages#legal'
+  
+  get 'selection' => 'pages#selection'
+  
+  get 'review' => 'pages#review'
+  
+  get 'shipping' => 'pages#shipping'
+  
+  get 'payment' => 'pages#payment'
+  
+  get 'success_payment' => 'pages#success_payment'
+  
+  get 'user_profile' => 'pages#user_profile'
+   
+  get 'artist_profile' => 'pages#artist_profile'
+  
+  get 'gallery_profile' => 'pages#gallery_profile'
 
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
